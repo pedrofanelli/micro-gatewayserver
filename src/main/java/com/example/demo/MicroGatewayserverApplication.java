dd4dabbf22928293906a40ee170cb00eff203287
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-25 11:39:18 -0300

    gateway server init

---
+++ src/main/java/com/example/demo/MicroGatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroGatewayserverApplication.class, args);
+	}
+
+}
